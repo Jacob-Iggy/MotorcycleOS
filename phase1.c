820146a743bd1b6cc924ed8710db4f29b1d20eea
Logan Pinel <loganpinel@outlook.com>
2026-03-09 17:32:15 -0400

    comments

--- phase1.c
+++ phase1.c
@@ -4,3 +4,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+//Engine State Thread
+
+//RPM Thread
+
+//RPM Zone
+
+//Engine Temp
+
+//Time
+
+//Speed
+
+//Fuel Level
+
+//Distance Traveled
+
+//Signal State (left/right)
+
+//Headlight (on/off)
+
+//TEAM OF THREE EXTRA COMPONENTS
+//Hybrid Assist Subsystem
+
+//Event Loggin Subsystem
+
+
+//MAIN FUNCTION
+int main() {
+
+}
